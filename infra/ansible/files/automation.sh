841c4d56cba130eb4328d7ab3b28caa4c931aaee
installer <installer@gmail.com>
2024-12-17 22:27:30 +0530

    fix: flock program

--- infra/ansible/files/automation.sh
+++ infra/ansible/files/automation.sh
@@ -4,4 +4,4 @@ LOCK_FILE="$(pwd)/doploy.lock"
 
 cd ~/services/project
 
-FLOCK -n $LOCK_FILE ./scripts/deploy_if_change.sh >> ~/automation/deploy.log 2>&1
+flock -n $LOCK_FILE ./scripts/deploy_if_change.sh >> ~/automation/deploy.log 2>&1
