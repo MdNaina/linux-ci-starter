fa7c593db56147b2337ba78c5255c59b92f83ed8
installer <installer@gmail.com>
2024-12-18 21:15:17 +0530

    fix

--- app/server.js
+++ app/server.js
@@ -6,14 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`)
-})
-
 app.get('/json', (req, res) => {
   const query = req.query
   res.json(query)
 })
+
+app.get('/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
